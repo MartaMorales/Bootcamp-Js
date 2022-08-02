1c4d1d763efed7e04f7fbb6c4414ccb32b0cb48f
marta <martamoralesfeito@gmail.com>
2022-08-02 23:59:26 +0200

    notacion, listas, objetos y fechas, ejercicio2

---
+++ Ejercicio2/Ejercicio2 .js
@@ -0,0 +1,16 @@
+const fecha_nacimiento = new Date(1989, 5, 5);
+const fecha_libro = new Date(1954, 6, 29)
+
+const ficha_personal = {
+    nombre: "Marta",
+    edad: 33,
+    desarrollador: true,
+    "fecha_nacimiento": fecha_nacimiento,
+    "libro_favorito":{
+        titulo: "El señor de los anillos",
+        autor: "J. R. R. Tolkien",
+        fecha: fecha_libro,
+        url: "https://books.google.es/books/about/Trilog%C3%ADa_El_Se%C3%B1or_de_los_Anillos.html?id=q_P7oMO3lC4C&printsec=frontcover&source=kp_read_button&hl=es&redir_esc=y#v=onepage&q&f=false"
+        }
+
+}
---
+++ Teoria/tipado-en-js-y-tipos-primitivos/notacion.js
@@ -0,0 +1,25 @@
+//Notacion
+//; sirve para delimitar el final de una linea
+
+const b = 4;
+
+//. -> se utiliza en los objetos para acceder a los atributos
+//movil.anchura
+
+//[] -> se utilizan para las listas, arrays
+const ar = [1,2,3,4];
+console.log(ar[2])
+
+//() -> se utilizan para funciones
+function suma(a,b){
+    //se escribe la funcion
+}
+
+//{} --> llaves para objetos, funciones y estructuras de control
+const movil = {
+    anchura: 5,
+    altura: 10
+}
+
+if(true){}
+
